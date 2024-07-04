74cab4e30e5eb74de7b79cfa596996401ff54881
ihc 06 <ciritci@gmail.com>
2024-07-05 02:23:10 +0300

    05-2

--- Python-MT/pyt1.py
+++ Python-MT/pyt1.py
@@ -1,9 +1,26 @@
 print ("merhaba python ben ihc")
 
-# 32:16 dk da kaldik  04/07/24
+# 32:16 dk da kaldik 
 # --------------
 input("isminiz nedir")
 input("Soyisminiz nedir")
 
-# --------------
+## --------------
+
+isim= "ih"
+sisim="C"
+print(isim+ " "+ sisim)
+
+
+isim,sisim= "AA", "bb"
+print(isim+ " "+ sisim)
+
+#----
+
+isim=input("isminiz nedir")
+Sisim=input("Soyisminiz nedir")
+
+##--------------------
+#52:40 da 05/07
+
 
