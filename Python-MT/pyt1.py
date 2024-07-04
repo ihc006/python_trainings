e1452daf49dcdd8dd4308807314b775c1eb009c0
ihc 06 <ciritci@gmail.com>
2024-07-05 02:13:32 +0300

    05-1

--- Python-MT/pyt1.py
+++ Python-MT/pyt1.py
@@ -1,3 +1,9 @@
-print("merhaba python ben ihc")
+print ("merhaba python ben ihc")
 
 # 32:16 dk da kaldik  04/07/24
+# --------------
+input("isminiz nedir")
+input("Soyisminiz nedir")
+
+# --------------
+
