4f7fc5823c10fe599191780da13c4ab7167e0b85
ihc 06 <ciritci@gmail.com>
2024-07-29 16:08:15 +0300

    pycharm o002

--- python-VHD/py001.py
+++ python-VHD/py001.py
@@ -1,15 +1,7 @@
 print("pycharm ile basladik bakalim")
 
-
-type(9);
-type(9.2);
-type("Aaa sensiz olmaaaz");
-
-
-# 50 steps
-
 #  Sayilar ve Stringler
 
 
 # pycharm ile basladik bakalimmmm   ooooo  1
-# pycharm github ayarlari ogrenildi
+
