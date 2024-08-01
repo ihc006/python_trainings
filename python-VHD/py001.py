08ba23677894e01cdedff4a2fa8f3d44ea42fe1f
ihc 06 <ciritci@gmail.com>
2024-08-01 10:01:02 +0300

    pyc 3107-01

--- python-VHD/py001.py
+++ python-VHD/py001.py
@@ -1,4 +1,34 @@
 print("pycharm ile basladik bakalim")
-
 #  Sayilar ve Stringler
 # Basladik 002
+# 31/07
+"a" * 3
+"a" + "b"
+"a" + "-b"
+
+a = 9
+b = 10
+a * b
+# mvk="Sample text"
+# del mvk
+a1t = "Sample text"
+
+len(a1t)
+
+a1t.upper()
+
+a1t.lower()
+
+a1t.islower()
+B=a1t.upper()
+print(B)
+
+a1t.isupper()
+B=a1t.lower()
+print(B)
+
+# replace
+
+
+
+# 01/08
