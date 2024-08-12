bfe1861185d5e487b2ac9d86ba1da4f3565dda54
ihc 06 <ciritci@gmail.com>
2024-08-13 02:04:15 +0300

    pyc 1308-02

--- python-VHD/py004.py
+++ python-VHD/py004.py
@@ -20,3 +20,22 @@ t2[1]
 t2[0:3]
 
 t2[2]=99   # degisiklik olmayacagina dair hata mesaji verir
+
+# Dictionary= Sozluk      veri yapilari
+
+sozlk={"Var1":"Python",
+       "Ver1":"3.10",
+       "Os1":"Windows 2022"}
+sozlk
+
+len(sozlk)  # goruldugu gibi burada 3 dger var.. sozluk tipinden dolayi
+
+sozlk2= {"v1":10,
+         "v2":20,
+         "v3":30 }
+sozlk2
+
+sozlk3= {"v1": ["dev",10],
+         "v2": ["vers",20,30],
+         "v3": "Windows"}
+sozlk3
