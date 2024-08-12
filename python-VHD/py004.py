776b09a45798d17fdd388a2e1a02694b3d191218
ihc 06 <ciritci@gmail.com>
2024-08-13 02:32:34 +0300

    pyc 1308-03

--- python-VHD/py004.py
+++ python-VHD/py004.py
@@ -39,3 +39,41 @@ sozlk3= {"v1": ["dev",10],
          "v2": ["vers",20,30],
          "v3": "Windows"}
 sozlk3
+
+# Sozluk eleman islemleri
+
+sozlk3= {"v1": ["dev",10],
+         "v2": ["vers",20,30],
+         "v3": "Windows"}
+
+sozlk3["v1"]
+sozlk3["v2"]
+
+
+sozlk4= {"v1": {"dev":10 , "os": 20},
+         "v2": {"dev":10 , "os": 20},
+         "v3": {"dev":10 , "os": 20}
+         }
+sozlk4
+
+sozlk4["v1"]["os"]  # v1 deki os sozluk degerini verir
+
+
+# Sozluk eleman ekleme ve cikarma
+sozlk4= {"v1": {"dev":10 , "os": 20},
+         "v2": {"dev":10 , "os": 20},
+         "v3": {"dev":10 , "os": 20}
+         }
+sozlk4
+
+sozlk4["v4"]= "dev"  # bir eleman ekledik
+
+sozlk4['v1']= "v1 in yeni degeri"
+sozlk4
+
+
+sozlk4[5]="son eklenen deger"  # bir deger ekelem tarzi
+sozlk4
+
+liste1= [1,2,3,4,5]
+liste1
