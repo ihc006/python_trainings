ee24a12a5deb311e8bee51d0c0d4877e96951a70
ihc 06 <ciritci@gmail.com>
2024-08-13 00:52:04 +0300

    pyc 1308-01

--- python-VHD/py004.py
+++ python-VHD/py004.py
@@ -1,3 +1,22 @@
 # 67  tuple - demet  veri yapilari
 
-t1=
+t1=("ali","veli",1,2,3.4,[1,2,3,4,5])
+
+t1="ali","veli",1,2,3.4,[1,2,3,4,5]
+
+
+# tuple
+
+t1=("eleman",)   # tek elemanli bir tuple olusturmak icin
+type(t1)
+
+
+####################################################
+# Tuple Eleman islemleri
+t2=("ali","veli",1,2,3,[1,2,3,4])
+
+t2[1]
+
+t2[0:3]
+
+t2[2]=99   # degisiklik olmayacagina dair hata mesaji verir
