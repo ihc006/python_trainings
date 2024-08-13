1115e1dd32023a1ca9d04790b3efa8c9afc6fef2
ihc 06 <ciritci@gmail.com>
2024-08-14 01:36:39 +0300

    pyc 1408-01

--- python-VHD/py005.py
+++ python-VHD/py005.py
@@ -1,1 +1,31 @@
 # 72 Setler
+
+
+s1= set()
+s1
+
+lst1=[1,"a","ali",123]  #liste
+lst1
+s2= set(lst1)
+s2
+
+tp1=("a","ali")  # tuple
+
+s3=set(tp1)
+s3
+
+ali="her_sey_guzel_olacak"
+type(ali)
+
+s= set(ali)
+
+veli=["ali","lutfen","ata","bakma","uzaya","git","git","ali","git","ali",]
+
+s=set(veli)
+s
+len(s)
+veli[0]  # liste deki 0 inci elemani verir
+
+s[0]   # 'se tip hatasi verir
+
+# 73 set elman ekleme cikarma
