628935005f21f6b5b8417349f8eeda47274dc075
ihc 06 <ciritci@gmail.com>
2024-08-15 20:03:23 +0300

    pyc 1508-07

---
+++ python-VHD/py007.py
@@ -0,0 +1,48 @@
+# 86 True False yapilari
+
+sinir1=5000
+
+sinir==4000 # mi derseniz False olarak doner
+
+# 87 if  yapilari
+
+sinir = 50000
+gelir = 60000
+
+gelir < sinir
+
+if gelir<sinir:
+    print("Geliriniz sinirdan kucuk")
+    print(gelir*2)
+
+if gelir>sinir:
+    print("Geliriniz sinirdan buyuk")
+    print(gelir*2)
+
+ # Else yapisi
+sinir = 50000
+gelir = 60000
+
+if gelir > sinir:
+    print("Geliriniz sinirdan buyuk")
+else :
+    print("Geliriniz sinirdan kucuk")
+
+if gelir < sinir:
+    print("Geliriniz sinirdan buyuk")
+else :
+    print("Geliriniz sinirdan kucuk degil")
+
+
+#
+
+sinir = 50000
+gelir = 51000
+
+if gelir == sinir:
+    print("Geliriniz sinir a esittir ")
+else :
+    print("Geliriniz sinir a esit degildir")
+
+
+# 89 elif
