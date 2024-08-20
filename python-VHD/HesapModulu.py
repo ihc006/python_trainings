9025a6a936726d494d9e8dbb419263659e087127
ihc 06 <ciritci@gmail.com>
2024-08-20 21:06:55 +0300

    pyc 2008-01

---
+++ python-VHD/HesapModulu.py
@@ -0,0 +1,8 @@
+#109 Module ornek
+from typing import List
+
+
+def new_salary(x):
+    print(x*1.2)
+
+maaslar = [1000,2000,3000,5000]   # bunu prog dan alamadik
--- python-VHD/py014-modul.py
+++ python-VHD/py014-modul.py
@@ -1,1 +1,39 @@
-# 109 modul olusturmak
+# 109 modul/paket /Kutuphane olusturmak
+
+import HesapModulu as hm
+
+hm.new_salary(1000)
+
+hm.new_salary(2000)
+
+from HesapModulu import new_salary
+
+new_salary(4000)
+
+print(str(hm.maaslar[0]))
+
+
+# 110  dataa / istisnalar (exception)
+
+a= 10
+b= 0
+
+a/b   # calistirildiginda division by zero hatasi alinir   Alinana hata sonrasi devam etmek icin
+
+
+try:
+    print(a/b)
+except ZeroDivisionError:
+    print("Payda da sifir olmaz")     # calisti program kirilmadi ve hatayi bize iletti
+
+
+
+a=10
+b="2"
+
+a/b   # TypeError hatasi aldik
+
+try:
+    print(a/b)
+except TypeError:
+    print(" Tip hatasi var")
