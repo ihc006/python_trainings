bb40ab5c5e669cac6cec16f17ee59add2d57c342
ihc 06 <ciritci@gmail.com>
2024-08-21 02:24:23 +0300

    pyc 2108-01

--- python-VHD/py015_numpy001.py
+++ python-VHD/py015_numpy001.py
@@ -1,3 +1,6 @@
 # 111  NumPy ve Pandas
 # 112  NumPy ve Pandas
 # 112 derstek' veri_manipulasyonu.ipynb.zip  dosyasi indirilir
+# 113 jupyterLab in kullanimini anlatiyor
+# 114 NumPy a giris  NumericalPython
+# 115 neden numpy
